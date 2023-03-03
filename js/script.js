ce1ebca39c4bb19186a1c5d174842764f90b1046
Mikaela Yasay <mikaelay@hawaii.edu>
2023-03-02 14:04:20 -1000

    added content and animation updates

--- js/script.js
+++ js/script.js
@@ -29,3 +29,9 @@ const myobserver = new IntersectionObserver((entries) => {
     myobserver.observe(section);
   });
   
+// LOGO HERO ANIMATION 
+const logo = document.querySelectorAll ("#logo path");
+
+for (let i = 0; i < logo.length; i++) {
+  console.log('Letter ${i} is ${logo[i].getTotalLength()}');
+}
